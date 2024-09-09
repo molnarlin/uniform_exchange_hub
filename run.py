56d2795417f9e4e77d7bfe609651208d188b8589
Katalin Molnar <molnarlin@gmail.com>
2024-09-09 22:46:00 +0000

    register.html added

--- run.py
+++ run.py
@@ -20,6 +20,10 @@ mongo = PyMongo(app)
 def index():
     return render_template("index.html")
 
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    return render_template("register.html")
+
 
 if __name__ == "__main__":
     app.run(
