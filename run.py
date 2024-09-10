e062f69d3b7243d01a56e61cbe3df9c7fb8c1811
Katalin Molnar <molnarlin@gmail.com>
2024-09-10 13:15:25 +0000

    login added

--- run.py
+++ run.py
@@ -43,6 +43,36 @@ def register():
         flash("Registration Successful!")
     return render_template("register.html")
 
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        # check if username exists in db
+        existing_user = mongo.db.users.find_one(
+            {"user_name": request.form.get("user_name").lower()})
+
+        if existing_user:
+            # ensure hashed password matches user input
+            if check_password_hash(
+                    existing_user["user_password"], request.form.get("user_password")):
+                        session["user"] = request.form.get("user_name").lower()
+                        flash("Welcome, {}".format(
+                            request.form.get("user_name")))
+                        return redirect(url_for(
+                            "profile", username=session["user"]))
+            else:
+                # invalid password match
+                flash("Incorrect Username and/or Password")
+                return redirect(url_for("login"))
+
+        else:
+            # username doesn't exist
+            flash("Incorrect Username and/or Password")
+            return redirect(url_for("login"))
+
+    return render_template("login.html")
+
+
 if __name__ == "__main__":
     app.run(
         host=os.environ.get("IP", "0.0.0.0"),
